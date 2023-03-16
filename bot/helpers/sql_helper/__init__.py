5675bcd8799d9c35b47c90d0ed0aa63b42269f16
Mr Khaine <118953358+khainee@users.noreply.github.com>
2023-03-16 13:26:07 +0630

    Update __init__.py

--- bot/helpers/sql_helper/__init__.py
+++ bot/helpers/sql_helper/__init__.py
@@ -1,5 +1,10 @@
 from pymongo import MongoClient
 from bot import DATABASE_URL, LOGGER
+import sys
+
+if DATABASE_URL is None:
+    LOGGER.warning("DATABASE_URL is not set. The application cannot function without a database.")
+    sys.exit(1)
 
 client = MongoClient(DATABASE_URL)
 db = client[DRIVE_X]
