0dc3a7c18be360559ed06a894c4bddbfc0e13b75
Mr Khaine <118953358+khainee@users.noreply.github.com>
2023-03-16 13:22:44 +0630

    Update __init__.py

--- bot/helpers/sql_helper/__init__.py
+++ bot/helpers/sql_helper/__init__.py
@@ -1,19 +1,7 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, scoped_session
+from pymongo import MongoClient
 from bot import DATABASE_URL, LOGGER
 
-
-def start() -> scoped_session:
-  try:
-    engine = create_engine(DATABASE_URL)
-    BASE.metadata.bind = engine
-    BASE.metadata.create_all(engine)
-    return scoped_session(sessionmaker(bind=engine, autoflush=False))
-  except ValueError:
-    LOGGER.error('Invalid DATABASE_URL : Exiting now.')
-    exit(1)
-
-
-BASE = declarative_base()
-SESSION = start()
+client = MongoClient(DATABASE_URL)
+db = client[DRIVE_X]
+parent_id = db["ParentID"]
+gDrive = db['gDriveCreds']
