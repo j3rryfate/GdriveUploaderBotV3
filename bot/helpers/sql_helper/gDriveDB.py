63dca9846ee43e684130e35d814cb578ab491881
Mr Khaine <118953358+khainee@users.noreply.github.com>
2023-03-16 13:27:22 +0630

    Update gDriveDB.py

--- bot/helpers/sql_helper/gDriveDB.py
+++ bot/helpers/sql_helper/gDriveDB.py
@@ -1,10 +1,6 @@
 import pickle
 import threading
-from pymongo import MongoClient
-
-client = MongoClient('mongodb://mongo:TpghbMFn6sO3JyPFzrRP@containers-us-west-148.railway.app:6601')
-db = client['your_database_name']
-collection = db['gDriveCreds']
+from bot.helpers.sql_helper import gDrive
 
 INSERTION_LOCK = threading.RLock()
 
@@ -13,12 +9,12 @@ def _set(chat_id, credential_string):
         filter_query = {'chat_id': chat_id}
         credential_string_pickle = pickle.dumps(credential_string)
         update_query = {'$set': {'credential_string': credential_string_pickle}}
-        collection.update_one(filter_query, update_query, upsert=True)
+        gDrive.update_one(filter_query, update_query, upsert=True)
 
 
 def search(chat_id):
     with INSERTION_LOCK:
-        saved_cred = collection.find_one({'chat_id': chat_id})
+        saved_cred = gDrive.find_one({'chat_id': chat_id})
         creds = None
         if saved_cred is not None:
             credential_string_pickle = saved_cred['credential_string']
@@ -28,4 +24,4 @@ def search(chat_id):
 
 def _clear(chat_id):
     with INSERTION_LOCK:
-        collection.delete_one({'chat_id': chat_id})
+        gDrive.delete_one({'chat_id': chat_id})
