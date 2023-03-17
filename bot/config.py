fa4ee2494179ffe9c433816a2c2593a856137342
j3rryfate <j3rryfate@hotmail.com>
2023-03-17 11:30:20 +0630

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -2,7 +2,7 @@ class config:
     BOT_TOKEN = "6215196852:AAFqwYc3WJ0iyxVPAJV-OhD-dmL5RVoracQ"
     APP_ID = "8802706"
     API_HASH = "97546900397a98f481bfb8252b1ac4f4"
-    DATABASE_URL = "PGPASSWORD=kpkaCaEVpNMgve3YeNhN psql -h containers-us-west-202.railway.app"
+    DATABASE_URL = "postgres://ltfttgsvyyixgu:21e43af8530ecc8d53d8055f625834e6d616d7df98863d56326802a4c14fe708@ec2-3-217-146-37.compute-1.amazonaws.com:5432/d6leu7rbo2d22u"
     SUDO_USERS = "1281749717" # Sepearted by space.
     SUPPORT_CHAT_LINK = ""
     DOWNLOAD_DIRECTORY = "./downloads/"
